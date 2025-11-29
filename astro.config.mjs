0e2c1875750b86274c806de983f32ff4fc33daa3
Brett Simpson <brett@codegravity.se>
2025-11-29 21:02:16 +0100

    menu fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [
+        mdx(), 
+        sitemap(), 
+        icon(),
+        robotsTxt()],
+    vite: {
+        plugins: [tailwindcss()],
+  	},
 });
