8fd67b50e2ce8a7136e483549b436201f6e02fbf
Brett Simpson <brett@codegravity.se>
2025-12-03 14:31:38 +0100

    added node

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,24 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import robotsTxt from 'astro-robots-txt';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    site: 'https://example.com',
-    integrations: [
-        mdx(), 
-        sitemap(), 
-        icon(),
-        robotsTxt()],
-    vite: {
-        plugins: [tailwindcss()],
-  	},
-    
+  output: 'server',
+  site: 'https://example.com',
+
+  integrations: [
+      mdx(), 
+      sitemap(), 
+      icon(),
+      robotsTxt()],
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
