c6026b7f0b484b75c873c0facffa2355fd75bc44
Brett Simpson <brett@codegravity.se>
2025-12-03 14:37:23 +0100

    fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  //output: 'hybrid',
   site: 'https://example.com',
 
   integrations: [
