b69f2e5293efa06d437a01ba9da987cbc2c29f82
Brett Simpson <brett@codegravity.se>
2026-01-12 20:26:16 +0100

    res och histori

---
+++ src/data/prisData.ts
@@ -0,0 +1,32 @@
+
+
+export const kursPriser26 = [ 
+    {
+        kursName: 'Grundkurs',
+        kursPris: 9500,
+        highFlight: 200,
+        transportBil: 80,
+        takeOffKit: 2020,
+        forsakringSSFF: 1750,
+        avbokning2v: 1000,
+        avbokning1v: 2000,
+    },
+]
+
+export const boendePriser26 = {
+    vinterDelad: 1500,
+    vinterEnkelt: 2200,
+    sommarDelad: 1500,
+    sommarEnkelt: 1950,
+    linneHanddukar: 200,
+    slutStad: 200,
+}
+
+export const liftPriser = {
+    vinterEnkeltTur:  180,
+    vinterDagsKort: 360,
+    vinterVeckoKort: 2000,
+    sommarEnkeltTur: 180,
+    sommarDagsKort: 360,
+    sommarVeckoKort: 2000,
+}
