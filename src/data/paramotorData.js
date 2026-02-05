0f7966eb200f79d8f69652b89808627a2466109c
Brett Simpson <brett@codegravity.se>
2026-02-05 12:27:22 +0100

    grund validate

---
+++ src/data/paramotorData.js
@@ -0,0 +1,21 @@
+export const paramotorKursData = [
+    {
+        paraGrundKursPris: '6000',
+        kursDates:[
+            {
+            week: '21',
+            datum: '2026-05-18',
+            },
+            {
+            week: '41',
+            datum: '2026-09-05',
+            },
+            {
+            week: '42',
+            datum: '2026-09-12',
+            },
+        ],
+
+    },
+
+];
