a627a8c5f1577783b4a26576db7417cc0e0b2b72
Brett Simpson <brett@codegravity.se>
2026-02-24 14:06:31 +0100

    pilots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
 import robotsTxt from 'astro-robots-txt';
-
 import node from '@astrojs/node';
-
+import react from '@astrojs/react';
 // Comment out Vercel for local dev
 import vercel from '@astrojs/vercel';
 
+
+import partytown from '@astrojs/partytown';
+
+
 export default defineConfig({
-  prefetch: true,
+    prefetch: true,
   
   site: 'https://stage.skysport.se',
 
@@ -22,27 +24,37 @@ export default defineConfig({
     mdx(),
     sitemap(),
     icon(),
-    robotsTxt()
+    robotsTxt(),
+    react(),
+    partytown({
+      config: { 
+        debug: false,
+        forward: ['dataLayer.push'],
+       },
+    }),
   ],
   experimental: {
     //
   },
   vite: {
     plugins: [tailwindcss()],
+    
     optimizeDeps: {
       include: ['leaflet'],
     },
   },
 
   //comment out  Vercel  for local dev
-    output: 'server',
+     output: 'server',
   adapter: vercel(),   
 
 // use node for local dev
 
-/*   output: 'static',
-adapter: node({
-    mode: 'standalone'
-  })  */ 
+//   output: 'server',
 
+  
+  
+  adapter: node({
+      mode: 'standalone'
+    })
 });
